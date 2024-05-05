90e0b34b472360a3cb714094075fb28005f2b67e
nayisntaround87 <naylasalwahayati@gmail.com>
2024-05-05 17:17:40 +0700

    first commit

---
+++ UCP1PEMDAS.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+using namespace std;
+
+
+struct Mahasiswa {
+    string nama;
+    string nilaimatematika;
+    string nilaibahasainggris;
+    int status;
+};
+
+int main(){
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Nilai Mahasiswa : ";
+    cin >> mhs.nilaimatematika;
+    cout << "nilai Mahasiswa : ";
+    cin >> mhs.nilaibahasainggris;
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.status;
+
+    cout << endl;
+    cout << "\n NIM :"<< mhs.nama;
+    cout << "\n Nama :"<< mhs.nilaimatematika;
+    cout << "\n Alamat :"<< mhs.nilaibahasainggris;
+    cout << "\n Umur :"<< mhs.status;
+
+
+    if (self) rata_rata () >= 70 or (self).nilai_matematika > 80: {
+
+            return "diterima"
+        (else):
+            return "ditolak"
+    }
+
+
+};
+
+
+
+
+
+// 1. string nama_buah = "Apel";
+//    int nPanjang = 4;
+//    float angka 1 = 4.1
+
+// 2. // int nomor;
+      // cout << "Masukan Nomor = ";
+      // cin >> nomor;
+
+      // if (nomor == 8) {
+      // cout << "tersebut angka 8" << endl;
+      // }
+      // cout << "finish";
+
+// 3. // struck pisang { 
+             // string rasa;
+             // float warna;
+             // int harga;
+             // string jenis;
+        // int main() {
+            // buah pisang;
+            // pisang rasa = "manis"
+            // pisang warna = kuing
+            // pisang harga = 12.000
+            // pisang jenis = pisang ambon
+
+            // cout << pisang rasa << endl;
+            // cin >> manis;
+            // cout << pisang warna << endl;
+            // cin >> kuning;
+            // cout << pisang harga << endl
+            // cin >> 12.000;
+            // cout << pisang jenis << endl;
+            // cin >> pisang ambon ;
+
+// 4.   // prosedur 
+        // void InputData() 
+        // {
+            // cout << "masukan bilangan pertama : ";
+            // cin >> bilangan 1;
+            // cout << "masukan bilangan kedua : ";
+            // cin >> bilangan 2;
+            // } 
+
+        // fungsi
+        // int penjumlahan
+        //{
+            // return a + b;
+        //}
+
+// 5. // int a = 1;
+      // do {
+        // cout << a << endl;
+           // a++;
+        // } while (a < = 1());
+
